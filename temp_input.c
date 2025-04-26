ac46e0f7fd2015ad419e376877cb1202a0eb990e
unknown <homp8355@gmail.com>
2025-04-26 14:21:51 +0300

    first commit

---
+++ temp_input.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main() {
+    int number;
+    long factorial;
+
+    printf("Введите число:\n");
+    scanf("%d", &number);
+    while (number!=-1) {
+       
+        factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial =factorial* i;
+        }
+        printf("Факториал равен %ld\n", factorial);
+        printf("Введите число");
+        scanf("%d", &number);
+        
+    }
+
+    return 0;
+}
